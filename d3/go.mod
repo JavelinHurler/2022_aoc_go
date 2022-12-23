7c909832008a12dd9608a9f8b660d2f6c2e06394
javelinhurler <rene.gerhard@freenet.de>
2022-12-23 13:56:05 +0100

    add folder for day 3

---
+++ d3/go.mod
@@ -0,0 +1,3 @@
+module example.com/2022_aoc_d3
+
+go 1.19
---
+++ d3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("os.Open: %s", err.Error())
+		os.Exit(-1)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		log.Println(line)
+	}
+}
